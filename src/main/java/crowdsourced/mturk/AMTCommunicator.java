749939fd12345a2623eb87350836f5256d815a19
Simon Rodriguez <kosua20@gmail.com>
2015-03-19 19:54:39 +0100

    #231: AddedAMTCommunicator class for sending tasks

---
+++ src/main/java/crowdsourced/mturk/AMTCommunicator.java
@@ -0,0 +1,18 @@
+package crowdsourced.mturk;
+/**
+ * The class dedicated to direct communications with AMT
+ * @author simon
+ *
+ */
+public class AMTCommunicator {
+
+	/**
+	 * Checks if the current account has a balance superior or equal to the passed amount
+	 * @param amount the minimum amount to have on the account
+	 * @return true if balance >= amount, false elsewhere.
+	 */
+	protected boolean checkBalance(float amount) {
+		return false;
+	}
+
+}
