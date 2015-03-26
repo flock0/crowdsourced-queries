80a60d88292ce80d416d8a6ed3aa2a06057589b7
Joachim Hugonot <joachim.hugonot@epfl.ch>
2015-03-26 13:38:59 +0100

    Skeleton for a simple query in place

--- src/main/scala/queryExecutor/Main.scala
+++ src/main/scala/queryExecutor/Main.scala
@@ -2,7 +2,7 @@ package queryExecutor
 
 object Main extends App {
   
-  val query = "SELECT (full name, NUMERIC age of death) FROM [Presidents of the USA] WHERE [political party is democrat] ORDER BY age of death"
+  val query = "SELECT (full name) FROM [Presidents of the USA]"// WHERE [political party is democrat] ORDER BY age of death"
   
   val queryExec = new QueryExecutor
   val parsedQuery = queryExec.parse(query)
--- src/main/scala/queryExecutor/QueryExecutor.scala
+++ src/main/scala/queryExecutor/QueryExecutor.scala
@@ -1,11 +1,21 @@
 package queryExecutor
 import parser.Parser
-
+import tree.Tree._
 class QueryExecutor() {
-  
-  def parse(query: String) = {
-    val parsedQuery = Parser.parseQuery(query)
-    println(parsedQuery)
-  }
+
+	def parse(query: String) = {
+		val parsedQuery = Parser.parseQuery(query)
+
+				parsedQuery.get match {
+
+				
+        case Select(nl, fields) => println(nl+" , "+fields)
+        
+        //case Join(left,right,on) => println("Join found")
+        //case Where(selectTree, where) => println("where")
+        case _ => println("shit")
+		}
+
+	}
 }
 
