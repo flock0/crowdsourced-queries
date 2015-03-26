8d66ca991b50de7b8544cf63ff544363cacc2c28
Joachim Hugonot <joachim.hugonot@epfl.ch>
2015-03-26 21:43:06 +0100

    recursiveTraversal added

--- src/main/scala/queryExecutor/Main.scala
+++ src/main/scala/queryExecutor/Main.scala
@@ -2,8 +2,9 @@ package queryExecutor
 
 object Main extends App {
   
-  val query = "SELECT (full name) FROM [Presidents of the USA]"// WHERE [political party is democrat] ORDER BY age of death"
-  
+  //val query = "SELECT (full name) FROM [Presidents of the USA] WHERE [political party is democrat] ORDER BY age of death"
+  val query = "SELECT (movies) FROM [Movies with Angelina Jolie] JOIN SELECT (movies) FROM [Movies with Brad Pitt] ON movies"
   val queryExec = new QueryExecutor
   val parsedQuery = queryExec.parse(query)
+  val executedQuery = queryExec.execute(parsedQuery)
 }
--- src/main/scala/queryExecutor/QueryExecutor.scala
+++ src/main/scala/queryExecutor/QueryExecutor.scala
@@ -1,21 +1,32 @@
 package queryExecutor
-import parser.Parser
+import parser.Parser	
 import tree.Tree._
+
 class QueryExecutor() {
-
-	def parse(query: String) = {
-		val parsedQuery = Parser.parseQuery(query)
-
-				parsedQuery.get match {
-
-				
-        case Select(nl, fields) => println(nl+" , "+fields)
-        
-        //case Join(left,right,on) => println("Join found")
-        //case Where(selectTree, where) => println("where")
-        case _ => println("shit")
-		}
-
+  
+	def parse(query: String): Q = {
+		return Parser.parseQuery(query).get
 	}
+  
+  def execute(query: Q) = {
+    
+    def recursiveTraversal(node: Q): Unit = node match{
+      
+        case Select(nl, fields) => recursiveTraversal(nl)
+        case Join(left,right,on) => recursiveTraversal(left); recursiveTraversal(right);
+        case Where(selectTree, where) => recursiveTraversal(selectTree)
+        case OrderBy(query, ascendingOrDescending, field) => recursiveTraversal(query) 
+        case NaturalLanguage(s) => println("Node found");
+        case _ => println("Problem"); 
+         
+    }
+      println("Starting execution of the query : \"" + query + "\"")
+      recursiveTraversal(query)
+  }
+  
+  def taskWhere(select: SelectTree, where: Condition)
+  def taskSelect(from: Q, elem: List[P])
+  def taskJoin(left: Q, right: Q, on: String) = 
+  def taskOrderBy(q: Q3, order: O, by: String)
+  
 }
-
