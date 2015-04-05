4ba5300fcae8378b5865272281e4c00e1b9bac8c
Florian Chlan <florian@chlan.net>
2015-04-05 20:00:58 +0200

    #264: Formatted and fixed polling task

--- src/main/java/crowdsourced/mturk/PollFinishedTimerTask.java
+++ src/main/java/crowdsourced/mturk/PollFinishedTimerTask.java
@@ -5,7 +5,6 @@ import java.io.StringReader;
 import java.security.SignatureException;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TimerTask;
@@ -35,50 +34,67 @@ public class PollFinishedTimerTask extends TimerTask {
      * The default number of HITs to fetch from one poll.
      */
     public static final int PAGE_SIZE = 100;
-    public static final long POLLING_FINISHED_RATE_MILLISECONDS = 10 * 1000; // At what rate should we poll whether the HIT is finished? This is only relevant after the lifetime of the HIT is over.
+    /**
+     * At what rate should we poll whether the HITs are finished?
+     */
+    public static final long POLLING_FINISHED_RATE_MILLISECONDS = 10 * 1000;
+    /**
+     * The task set to work on.
+     */
     private AMTTaskSet taskSet;
-    private DocumentBuilder docBuilder;
+    /**
+     * The total number of pages of reviewable HITs for the most recent retrieval operation.
+     */
     private int totalNumberOfPages;
-    
-    public PollFinishedTimerTask(AMTTaskSet taskSet) {
-         this.taskSet = taskSet;
-         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-         try {
-             docBuilder = docFactory.newDocumentBuilder();
-         } catch (ParserConfigurationException e) {
-             throw new RuntimeException(
-                     "Could not initialize document builder: " + e.toString());
-         }
+    private DocumentBuilder docBuilder;
+
+    public PollFinishedTimerTask(AMTTaskSet _taskSet) {
+        this.taskSet = _taskSet;
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        try {
+            docBuilder = docFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(
+                    "Could not initialize document builder: " + e.toString());
+        }
     }
+
     @Override
     public void run() {
         try {
             boolean lastResponseWasValid;
             int currentPageNumber = 1;
+            /* Retrieve all pages of reviewable HITs one-by-one */
             Set<String> reviewableHITs = new HashSet<>();
             do {
                 String response = getReviewableHITs(currentPageNumber);
                 Document doc = docBuilder.parse(new InputSource(new StringReader(response)));
                 lastResponseWasValid = responseIsValid(doc);
-    
+
                 if (lastResponseWasValid) {
-    
+
                     Set<String> hits = extractReviewableHITs(doc);
                     reviewableHITs.addAll(hits);
                     currentPageNumber++;
                 }
             } while (lastResponseWasValid && currentPageNumber <= totalNumberOfPages);
-            
+
+            /* Check if any of the active tasks is reviewable. This means, that they are finished. */
             for (AMTTask t : taskSet.getActiveTasks()) {
                 if (reviewableHITs.contains(t.getHIT().getHITId())) {
-                    t.finishJob();
+                    t.finishTask();
                 }
             }
         } catch (IOException | SAXException | XPathExpressionException | SignatureException ex) {
             System.out.println("Polling for reviewable HITs has failed: " + ex.getMessage());
         }
     }
-    
+
+    /**
+     * Retrieves one page of reviewable HITs.
+     * @param pageNumber The page number to retrieve.
+     * @return The answer from AMT.
+     */
     private String getReviewableHITs(int pageNumber) throws SignatureException, IOException {
         Map<String, String> param = new HashMap<String, String>();
         param.put("Operation", "GetReviewableHITs");
@@ -107,12 +123,17 @@ public class PollFinishedTimerTask extends TimerTask {
             return false;
         }
     }
+
+    /**
+     * Extracts the individual HITIds from the document.
+     * @param doc THE XML-document received by AMT.
+     * @return A set of individual HITIds that are reviewable.
+     */
     private Set<String> extractReviewableHITs(Document doc) throws XPathExpressionException {
-        
         XPathFactory xPathFac = XPathFactory.newInstance();
         XPath xPath = xPathFac.newXPath();
 
-        /* If there are more assignments than can fit on this page, fetch more after this round */
+        /* If there are more HITs than can fit on this page, fetch more after this round */
         String numResultsString = xPath
                 .compile("/GetReviewableHITsResponse/GetReviewableHITsResult/NumResults")
                 .evaluate(doc);
@@ -121,21 +142,21 @@ public class PollFinishedTimerTask extends TimerTask {
                 .evaluate(doc);
         int numResults = Integer.parseInt(numResultsString);
         int totalNumResults = Integer.parseInt(totalNumResultsString);
-        totalNumberOfPages = (totalNumResults + PAGE_SIZE/2 + 1) / PAGE_SIZE; 
-        
+        totalNumberOfPages = (totalNumResults + PAGE_SIZE / 2 + 1) / PAGE_SIZE;
+
         Set<String> result = new HashSet<>();
         if (numResults > 0) {
             NodeList hits = (NodeList) xPath.compile(
                     "/GetReviewableHITsResponse/GetReviewableHITsResult/HIT")
                     .evaluate(doc, XPathConstants.NODESET);
-            
+
             /* Go through all the reviewable HITs received */
             for (int i = 0; i < numResults; i++) {
                 Node n = hits.item(i);
                 result.add(xPath.compile("./HITId").evaluate(n));
             }
         }
-        
+
         return result;
     }
 }
