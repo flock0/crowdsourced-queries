0d84987bfca7fdc1888eb713b16077e58269451d
Florian Vessaz <florian@florv.ch>
2015-04-14 15:48:26 +0200

    Serve static document from JAR file or resources dir
    Whether one or the other is done is determined at compile time (see
    HTTPServer.java). Serving resources from resources directory is only
    convenient during development since it allows to change documents
    without recompiling the project but the PWD must be the root of the
    source directory for this to work (which is inconvenient when deploying
    the app).

--- src/main/java/crowdsourced/http/HTTPServer.java
+++ src/main/java/crowdsourced/http/HTTPServer.java
@@ -1,9 +1,13 @@
 package crowdsourced.http;
 
+import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Response;
+import org.glassfish.grizzly.http.server.StaticHttpHandler;
+
+
 
 import java.util.Date;
 import java.text.SimpleDateFormat;
@@ -20,6 +24,11 @@ public class HTTPServer implements Runnable {
     private HttpServer server;
     private final static int SERVER_PORT = 8080;
 
+    /* Set BUILTIN_RESOURCES to true to serve static files from the jar file
+     * instead of looking for them in BASE_DIR. */
+    private final static boolean BUILTIN_RESOURCES = false;
+    private final static String BASE_DIR = "./src/main/resources";
+
     /** Create a new HTTP server.
      * @param _qif The QueryInterface used to answer requests.
      */
@@ -27,7 +36,19 @@ public class HTTPServer implements Runnable {
         this.qif = _qif;
         this.server = HttpServer.createSimpleServer("/dev/null", SERVER_PORT);
 
-        server.getServerConfiguration().addHttpHandler(new RootHandler(), "/");
+        server.getServerConfiguration().addHttpHandler(new TimeHandler(), "/");
+        server.getServerConfiguration().addHttpHandler(new TimeHandler(), "/time");
+
+        /*
+        if (BUILTIN_RESOURCES) {
+            server.getServerConfiguration().addHttpHandler(
+                    new CLStaticHttpHandler(HTTPServer.class.getClassLoader()),
+                    "/");
+        } else {
+            server.getServerConfiguration().addHttpHandler(
+                    new StaticHttpHandler(BASE_DIR), "/");
+        }
+        */
     }
 
     /** Starts the webserver. */
@@ -39,8 +60,19 @@ public class HTTPServer implements Runnable {
         }
     }
 
-    /** The HttpHandler for / */
+    /** HttpHandler for / */
     public class RootHandler extends HttpHandler {
+        public void service(Request request, Response response) throws Exception {
+            String date =
+                new SimpleDateFormat("yyyy-MM-dd HH:mm:ssXXX").format(new Date());
+            response.setContentType("text/plain");
+            response.setContentLength(date.length());
+            response.getWriter().write(date);
+        }
+    }
+
+    /** Example handler that return a text page with current date and time */
+    public class TimeHandler extends HttpHandler {
         public void service(Request request, Response response) throws Exception {
             String date =
                 new SimpleDateFormat("yyyy-MM-dd HH:mm:ssXXX").format(new Date());
