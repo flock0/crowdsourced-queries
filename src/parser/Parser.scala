e07dad2ec9dc34dbd2a32862814b39d0ee9b6255
chernobyl88 <v.z@paragp.ch>
2015-04-22 21:26:10 +0200

    first try on modif

--- src/parser/Parser.scala
+++ src/parser/Parser.scala
@@ -1,33 +1,36 @@
 package parser
 import tree.Tree._
 import scala.util.parsing.combinator.RegexParsers
-//SELECT (movies) FROM « Movies with Angelina Jolie » JOIN SELECT (movies) FROM « Movies with Brad Pitt » ON movies
-//
-//SELECT (full name, NUMERIC age of death) FROM « Presidents of the USA » WHERE « political party is democrat » ORDER BY age of death
+
 object Parser extends RegexParsers with java.io.Serializable{
   
 	def parseQ: Parser[Q] = (
 	  parseQ1 ~ "JOIN" ~ parseQ1 ~ "ON" ~ parseE ^^ {case left ~ _ ~ right ~ _ ~ on => Join(left, right, on)}
 	  | parseQ1 ^^ {case q1 => q1}
+    | failure("Error")
 	)
 	
 	def parseQ1: Parser[Q1] = (
 	  parseQ2 ^^ {case q2 => q2}
+    | failure("Error")
 	)
 	
 	def parseQ2: Parser[Q2] = (
 	  parseQ3~"ORDER BY"~parseE~"DESC" ^^ {case q~_~e~o => OrderBy(q, DESC(), e)}
 	  | parseQ3~"ORDER BY"~parseE~opt("ASC") ^^ {case q~_~e~_ => OrderBy(q, ASC(), e)}
 	  | parseQ3 ^^ {case q3 => q3}
+    | failure("Error")
 	)
 	
 	def parseQ3: Parser[Q3] = (
 	  parseSelectTree ~ "WHERE" ~ parseC ^^ {case select~_~c => Where(select, c)}
 	  | parseSelectTree ^^{case q4 => q4}
+    | failure("Error")
 	)
 	
 	def parseSelectTree: Parser[SelectTree] = (
 	  "SELECT (" ~ parseP ~ ") FROM" ~ parseNl ^^ {case _~p~_~e => Select(e, p)}
+    | failure("Error")
 	  //| parseNL ^^ (case nl => nl)
 	)
 	
