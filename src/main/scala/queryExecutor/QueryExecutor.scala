a49e50de6c5fc5a7fb1d4d5917225ca9b3c5bec8
Szabo Kristof Tamas <kristoftamas.szabo@epfl.ch>
2015-05-10 12:18:07 +0200

    added support for LIMIT keyword

--- src/main/scala/queryExecutor/QueryExecutor.scala
+++ src/main/scala/queryExecutor/QueryExecutor.scala
@@ -47,14 +47,18 @@ class QueryExecutor(val queryID: Int, val queryString: String) {
   /**
    * Construct the hierarchy of all requests and the chaining of tasks based on the parsed tree
    */
-  private def startingPoint(node: RootNode): List[Future[List[Assignment]]] = node match {
+  private def startingPoint(node: RootNode, limit: Int = DEFAULT_ELEMENTS_SELECT): List[Future[List[Assignment]]] = node match {
       // TODO we need to pass a limit to taskSelect. The dataset could be very small or huge...
       // TODO maybe get this from the request using the LIMIT keyword. Or ask a worker for number of elements in the web page
-      case Select(nl, fields) => taskSelect(nl, fields)
+      case Select(nl, fields) => taskSelect(nl, fields, limit)
       case Join(left, right, on) => taskJoin(left, right, on) //recursiveTraversal(left); recursiveTraversal(right);
       case Where(selectTree, where) => taskWhere(selectTree, where)
       case OrderBy(query, List(ascendingOrDescending)) => taskOrderBy(query,ascendingOrDescending) //TODO
       case Group(query,by)=>taskGroupBy(query,by)
+      case Limit(query, limit) => limit match {
+        case IntL(i) => startingPoint(query, i)
+        case _ => startingPoint(query)
+      }
       case _ => List[Future[List[Assignment]]]() //TODO
     }
 
@@ -127,7 +131,7 @@ class QueryExecutor(val queryID: Int, val queryString: String) {
   /**
    * Creation of SELECT Task
    */
-  def taskSelect(from: RootNode, fields: List[Operation]): List[Future[List[Assignment]]] = {
+  def taskSelect(from: RootNode, fields: List[Operation], limit: Int = DEFAULT_ELEMENTS_SELECT): List[Future[List[Assignment]]] = {
     println("Task select started")
 
     val taskID = generateUniqueID()
@@ -142,8 +146,8 @@ class QueryExecutor(val queryID: Int, val queryString: String) {
     
     println(extractNodeAnswers(from, nl).mkString(", "))
     val tasks = RETRIEVE_PRIMARY_KEY_FIRST match {
-      case false => TasksGenerator.selectTasksGenerator(extractNodeAnswers(from, nl).head, from.toString, fields, MAX_ELEMENTS_PER_WORKER, DEFAULT_ELEMENTS_SELECT)
-      case true => TasksGenerator.selectPrimaryKeyTasksGenerator(extractNodeAnswers(from, nl), from.toString, fields, MAX_ELEMENTS_PER_WORKER, DEFAULT_ELEMENTS_SELECT)
+      case false => TasksGenerator.selectTasksGenerator(extractNodeAnswers(from, nl).head, from.toString, fields, MAX_ELEMENTS_PER_WORKER, limit)
+      case true => TasksGenerator.selectPrimaryKeyTasksGenerator(extractNodeAnswers(from, nl), from.toString, fields, MAX_ELEMENTS_PER_WORKER, limit)
     }
     tasks.foreach(_.exec)
     status.addTasks(tasks)
@@ -287,11 +291,15 @@ class QueryExecutor(val queryID: Int, val queryString: String) {
   /**
    * Helper function when nodes have left and right parts
    */
-  private def executeNode(node: RootNode): List[Future[List[Assignment]]] = {
+  private def executeNode(node: RootNode, limit: Int = DEFAULT_ELEMENTS_SELECT): List[Future[List[Assignment]]] = {
     node match {
-    case Select(nl, fields) => taskSelect(nl, fields)
+    case Select(nl, fields) => taskSelect(nl, fields, limit)
     case Join(left, right, on) => taskJoin(left, right, on)
     case Where(selectTree, where) => taskWhere(selectTree, where)
+    case Limit(query, limit) => limit match {
+        case IntL(i) => startingPoint(query, i)
+        case _ => startingPoint(query)
+      }
     case _ => ???
     }
   }
